38bd903788dce49c38971134b19b0b7999032008
Naila Atresia Rahma <nailaarahma1912@gmail.com>
2025-09-29 10:02:07 +0700

    Upload C File

---
+++ PRAK002-2510817120014-NailaAtresiaRahma.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("Selamat Pagi, Naila Atresia Rahma\nSelamat Siang, Naila Atresia Rahma\nSelamat Malam, Naila Atresia Rahma");
+   
+    return 0;
+}
---
+++ PRAK004-251087120014-NailaAtresiaRahma.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("############################## \n");
+    printf("#                            # \n");
+    printf("#    Naila Atresia Rahma     # \n");
+    printf("#        2510817120014       # \n");
+    printf("#                            # \n");
+    printf("##############################");
+    return 0;
+}
